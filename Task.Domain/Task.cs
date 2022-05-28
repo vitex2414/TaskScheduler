30454a06ba5dd9748a9497c9edf768bb45870c04
victor <victor@DESKTOP-0BH6R8E>
2022-05-29 05:40:43 +0600

    Добавьте файлы проекта.

---
+++ Task.Domain/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Domain
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Modified_Date { get; set; }
+        public DateTime Due_Date { get; set; }
+       
+    }
+}
---
+++ Task.Domain/TaskStory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.Domain
+{
+    public class TaskStory
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string OperationType { get; set; }  
+        public DateTime History_Date { get; set; }
+      
+    }
+}
